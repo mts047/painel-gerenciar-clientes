067c9fe0e095e8b02b741c0f88aa833ba63a69e8
mts047 <144283827+mts047@users.noreply.github.com>
2023-09-21 22:03:28 -0300

    processar-formulario.php

--- processar-formulario.php
+++ processar-formulario.php
@@ -1,46 +1,34 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Lista de Clientes</title>
-    <!-- Inclua o link para o seu arquivo CSS, se necessário -->
-</head>
-<body>
-    <header>
-        <!-- Conteúdo do cabeçalho -->
-    </header>
-    <div class="container">
-        <h2>Lista de Clientes</h2>
-        <table>
-            <thead>
-                <tr>
-                    <th>Nome</th>
-                    <th>Whatsapp</th>
-                    <th>Email</th>
-                    <th>Data de Ativação</th>
-                    <th>Data do Último Pagamento</th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php
-                // Certifique-se de que o arquivo clientes.txt existe no diretório data
-                $clientes = file("data/clientes.txt", FILE_IGNORE_NEW_LINES);
-                if ($clientes) {
-                    foreach ($clientes as $cliente) {
-                        $dados = explode(", ", $cliente);
-                        echo "<tr>";
-                        foreach ($dados as $dado) {
-                            echo "<td>$dado</td>";
-                        }
-                        echo "</tr>";
-                    }
-                } else {
-                    echo "<tr><td colspan='5'>Nenhum cliente encontrado.</td></tr>";
-                }
-                ?>
-            </tbody>
-        </table>
-    </div>
-</body>
-</html>
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Obter os dados do formulário
+    $nome = $_POST["nome"];
+    $whatsapp = $_POST["whatsapp"];
+    $email = $_POST["email"];
+    $dataAtivacao = $_POST["data-ativacao"];
+    $dataPagamento = $_POST["data-pagamento"];
+
+    // Formatar os dados em uma linha para salvar no arquivo
+    $linha = "$nome, $whatsapp, $email, $dataAtivacao, $dataPagamento" . PHP_EOL;
+
+    // Abrir o arquivo "clientes.txt" para escrita
+    $arquivo = fopen("data/clientes.txt", "a"); // "a" para adicionar ao arquivo existente
+
+    if ($arquivo) {
+        // Escrever a linha no arquivo
+        fwrite($arquivo, $linha);
+
+        // Fechar o arquivo
+        fclose($arquivo);
+
+        // Redirecionar de volta para a página de adicionar cliente com uma mensagem de sucesso
+        header("Location: adicionar-cliente.html?success=1");
+        exit;
+    } else {
+        // Lidar com erros ao abrir o arquivo
+        echo "Erro ao abrir o arquivo.";
+    }
+} else {
+    // Lidar com solicitações que não sejam POST (ou seja, acesso direto a este script)
+    echo "Acesso não autorizado.";
+}
+?>
